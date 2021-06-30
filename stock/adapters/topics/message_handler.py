3ab374e5f7816f5abd4607cfd6a8025dfc5ef113
Max Kossatz <mkossatz@redhat.com>
2021-06-30 20:07:31 +0100

    stock: adapters: topics: implement message handling and shelves topics

---
+++ stock/adapters/topics/message_handler.py
@@ -0,0 +1,31 @@
+import logging
+from proton.handlers import MessagingHandler
+from proton import Message
+
+
+class MessageHandler(MessagingHandler):
+    def __init__(self, host: str, address: str, message_body: str):
+        super(MessageHandler, self).__init__()
+        self.conn_url: str = host
+        self.address: str = address
+        self.message_body: str = message_body
+
+    def on_start(self, event):
+        conn = event.container.connect(self.conn_url)
+        event.container.create_sender(conn, self.address)
+
+    def on_link_opened(self, event):
+        logging.debug("MessageHandler.on_link_opened:Opened sender for target address '{0}'".format
+                      (event.sender.target.address))
+
+    def on_sendable(self, event):
+        message = Message(self.message_body)
+        event.sender.send(message)
+        logging.debug(
+            "MessageHandler.on_sendable:Sent message '{0}'".format(message.body))
+        event.sender.close()
+        logging.debug(
+            "MessageHandler.on_sendable:Closed sender")
+        event.connection.close()
+        logging.debug(
+            "MessageHandler.on_sendable:Closed connection")
--- stock/adapters/topics/shelves_topics.py
+++ stock/adapters/topics/shelves_topics.py
@@ -1,26 +1,61 @@
+from __future__ import print_function
 import logging
+from proton.reactor import Container
 
 from stock.core.interfaces.shelves_topics import ShelvesTopicsInterface
 from stock.core.messages.restocked import Restocked
 from stock.core.messages.depleted import Depleted
 from stock.core.messages.restock_threshhold_reached import RestockThresholdReached
 from stock.core.messages.registered_shelve import RegisteredShelve
+from stock.adapters.topics.message_handler import MessageHandler
 
 
 class ShelvesTopics(ShelvesTopicsInterface):
 
+    def __init__(self, host: str):
+        self._host = host
+
     def send_restocked_message(self, message: Restocked):
+        # Container(MessageHandler(
+        #     self._host, "shelveRestocksAddress", message.to_json())
+        # ).run()
         logging.warning(
             "ShelvesTopics.send_restocked_message:Not implemented")
 
     def send_depleted_message(self, message: Depleted):
+        # Container(MessageHandler(
+        #     self._host, "shelveDepletionsAddress", message.to_json())
+        # ).run()
         logging.warning(
             "ShelvesTopics.send_depleted_message:Not implemented")
 
     def send_restock_threshold_reached_message(self, message: RestockThresholdReached):
-        logging.warning(
-            "ShelvesTopics.send_restock_threshold_reached_message:Not implemented")
+        Container(MessageHandler(
+            self._host, "shelveRestockThresholdReachedAddress", message.to_json())
+        ).run()
 
     def send_registered_shelve_message(self, message: RegisteredShelve):
+        # Container(MessageHandler(
+        #     self._host, "shelveRegistrationsAddress", message.to_json())
+        # ).run()
         logging.warning(
             "ShelvesTopics.send_registered_shelve_message:Not implemented")
+
+
+class ShelvesTopicsDisabled(ShelvesTopicsInterface):
+
+    def send_restocked_message(self, message: Restocked):
+        logging.info(
+            "ShelvesTopics.send_restocked_message:Disabled")
+
+    def send_depleted_message(self, message: Depleted):
+        logging.info(
+            "ShelvesTopics.send_depleted_message:Disabled")
+
+    def send_restock_threshold_reached_message(self, message: RestockThresholdReached):
+        logging.info(
+            "ShelvesTopics.send_restock_threshold_reached_message:Disabled")
+
+    def send_registered_shelve_message(self, message: RegisteredShelve):
+        logging.info(
+            "ShelvesTopics.send_registered_shelve_message:Disabled")
